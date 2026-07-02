075478ab5f071037a0cf0534a603667884f59052
sravani192019 <sravani.jamithireddy@gmail.com>
2026-07-03 01:00:07 +0530

    new

--- locals.tf
+++ locals.tf
@@ -3,7 +3,7 @@ locals {
     private_subnet_id= split(",", data.aws_ssm_parameter.private_subnet_id.value)[0]
     ami_id = data.aws_ami.joindevops.id
     vpc_id = data.aws_ssm_parameter.vpc_id.value
-    #backend_lb_listener_arn = data.aws_ssm_parameter.backend_lb_listener_arn.value
+    backend_lb_listener_arn = data.aws_ssm_parameter.backend_lb_listener_arn.value
     common_tags = {
         project = var.project
         environment = var.environment
